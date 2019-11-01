20a2777be811339a4c2afba2b3f99ba42f9d7748
mpbon <mattpaley@hotmail.com>
2019-10-31 20:10:51 -0600

    initial commit

---
+++ resources/views/post/create.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Create!</title>
+    </head>
+    <body>
+
+        <form class="" action="{{route('post.store') }}" method="post">
+            <label for="">
+                Title
+                <input type="text" name="title" value="{{old('title') }}" placeholder="Enter a post title...">
+            </label>
+            <label for="content">
+                Title
+                <input type="text" name="title" value="{{old('title') }}" placeholder="Enter a post title...">
+            </label>
+            <label for="">
+                
+            </label>
+        </form>
+
+    </body>
+</html>
